a4a94c3ee0aa0dd1ba84aed01132c9cfa57c0be9
Rubén Antón <rubocoptero@gmail.com>
2015-06-13 22:17:00 +0200

    Task are created.

--- src/Task.js
+++ src/Task.js
@@ -1,6 +1,6 @@
 
 module.exports = {
     new: function () {
-
+        return true;
     }
 };
--- src/TaskWriteService.js
+++ src/TaskWriteService.js
@@ -1,6 +1,9 @@
-module.exports = {
-    createNewTag: function () {
+var TaskRepository = require('./TaskRepository.js'),
+    Task = require('./Task.js');
 
+module.exports = {
+    createNewTag: function (name) {
+        TaskRepository.save(Task.new(name));
     }
 };
 
